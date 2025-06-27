e0b6c41dfb2c8717e14cce244002eee3edadf9fc
Carlos <hen9@ymail.com>
2025-06-27 18:12:22 -0300

    atualizando

--- config/bd.class.php
+++ config/bd.class.php
@@ -1,34 +1,31 @@
 <?php
-class Banco{
+class Banco
+{
     private static $dbNome = 'pequia';
     private static $dbHost = 'localhost';
     private static $dbUsuario = 'root';
     private static $dbSenha = '';
-    
+
     private static $cont = null;
-    
-    public function __construct() 
+
+    public function __construct()
     {
         die('A função Init nao é permitido!');
     }
-    
+
     public static function conectar()
     {
-        if(null == self::$cont)
-        {
-            try
-            {
-                self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbNome, self::$dbUsuario, self::$dbSenha); 
-            }
-            catch(PDOException $exception)
-            {
-                echo 'error do banco '.$exception->getMessage(); 
+        if (null == self::$cont) {
+            try {
+                self::$cont = new PDO("mysql:host=" . self::$dbHost . ";" . "dbname=" . self::$dbNome, self::$dbUsuario, self::$dbSenha);
+            } catch (PDOException $exception) {
+                echo 'error do banco ' . $exception->getMessage();
                 die($exception->getMessage());
             }
         }
         return self::$cont;
     }
-    
+
     public static function desconectar()
     {
         self::$cont = null;
